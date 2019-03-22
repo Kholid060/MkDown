9c7594aece314720e5fdbef46ab5dbab846564b8
kholid060 <kholid060@gmail.com>
2019-03-22 16:15:11 +0800

    first commit

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
+import './plugins/vuetify'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import VuePrismEditor from 'vue-prism-editor'
+import 'vue-prism-editor/dist/VuePrismEditor.css'
+Vue.component('prism-editor', VuePrismEditor)
 
 Vue.config.productionTip = false
 
---
+++ src/views/editor.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <v-toolbar dark color="#3d404d" dense class="elevation-0">
+      <v-toolbar-side-icon></v-toolbar-side-icon>
+      <v-toolbar-title class="white--text">Title</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items>
+        <v-btn flat>halo</v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+    <v-layout row column>
+      <v-card color="#e8e8e8" class="elevation-4">
+        <v-flex md12>
+          <v-layout row wrap>
+            <v-flex md6 my-2>
+              1
+            </v-flex>
+            <v-flex md6 pa-3 pr-5>
+              <v-text-field class="file-name-form" full-width hide-details outline value="untitled document" label="File name"></v-text-field>
+            </v-flex>
+          </v-layout>
+        </v-flex>
+      </v-card>
+      <v-flex md12>
+        3
+      </v-flex>
+    </v-layout>
+  </div>
+</template>
+<script>
+export default {
+  data: () => ({
+    test: 'anu'
+  })
+}
+</script>
+
+<style>
+.file-name-form .v-input__control .v-input__slot {
+  background-color: rgb(217, 217, 217) !important;
+  border-color: rgb(203, 203, 203) !important;
+}
+</style>
